3618270110e70d176449ee3cd51ed11530af55ca
Gabriel Sanchez <gisgarci2@hotmail.com>
2024-09-19 20:22:06 -0600

    Adding routes with react-router-dom in App.js page

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -1,14 +1,30 @@
-import './App.css';
-import { Account } from '../Home';
-import { Home } from '../Home';
-import { MyOrders } from '../MyOrders';
-import { NotFound } from '../NotFound';
-import { Order } from '../Order';
-import { SignIn } from '../SignIn';
+import { useRoutes, BrowserRouter } from 'react-router-dom'
+import { Account } from '../Account'
+import { Home } from '../Home'
+import { MyOrders } from '../MyOrders'
+import { NotFound } from '../NotFound'
+import { Order } from '../Order'
+import { SignIn } from '../SignIn'
+import './App.css'
 
-function App() {
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: '/', element: <Home/> },
+    { path: '/account', element: <Account/> },
+    { path: '/order', element: <Order/> },
+    { path: '/my-orders', element: <MyOrders/> },
+    { path: '/sign-in', element: <SignIn/> },
+    { path: '/*', element: <NotFound/> },
+  ])
+
+  return routes;
+}
+
+const App = () => {
   return (
-    <Home/>
+    <BrowserRouter>
+      <AppRoutes/>
+    </BrowserRouter>
   )
 }
 
